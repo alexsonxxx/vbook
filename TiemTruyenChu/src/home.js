5d76a0c9e8bd922a37dc5bec9c28f0c2fe4b50bf
alexsonxxx <rumskiee02@gmail.com>
2026-02-13 14:20:36 +0700

    Delete TiemTruyenChu directory

--- TiemTruyenChu/src/home.js
+++
@@ -1,8 +0,0 @@
-function execute() {
-    return Response.success([
-        {title: "Mới cập nhật", input: "/danh-sach?sort=updated", script: "gen.js"},
-        {title: "Hot", input: "/xep-hang?by=views", script: "gen.js"},
-        {title: "Truyện Nam", input: "/danh-sach?gender=nam", script: "gen.js"},
-        {title: "Truyện Nữ", input: "/danh-sach?gender=nu", script: "gen.js"}
-    ]);
-}
