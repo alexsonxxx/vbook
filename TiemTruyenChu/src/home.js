f3f2207e56c7e8ae67b7c912e8bb28806fd5f06a
alexsonxxx <rumskiee02@gmail.com>
2026-02-13 13:56:31 +0700

    Update home.js

--- TiemTruyenChu/src/home.js
+++ TiemTruyenChu/src/home.js
@@ -1,7 +1,7 @@
 function execute() {
     return Response.success([
         {title: "Mới cập nhật", input: "/danh-sach?sort=updated", script: "gen.js"},
-        {title: "Hot", input: "/danh-sach?sort=views", script: "gen.js"},
+        {title: "Hot", input: "/xep-hang?by=views", script: "gen.js"},
         {title: "Truyện Nam", input: "/danh-sach?gender=nam", script: "gen.js"},
         {title: "Truyện Nữ", input: "/danh-sach?gender=nu", script: "gen.js"}
     ]);
